e75b17c327fcf849a5edfae5f9ab78e40b606b33
PerezVitor <vitor.g.perez@hotmail.com>
2022-01-27 15:26:18 -0300

    Adicionando suporte a controllers no Builder da aplicação

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    }); 
+
+builder.Services.AddDbContext<Blog.Data.BlogDataContext>();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
