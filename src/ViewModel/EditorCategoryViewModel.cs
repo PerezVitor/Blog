1cf0222b089393b2bb6481cd4ad083aadf92f53c
PerezVitor <vitor.g.perez@hotmail.com>
2022-01-27 16:46:13 -0300

    Criação das ViewModels (retorno da requisição e Editor da categoria)

---
+++ src/ViewModel/EditorCategoryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModel
+{
+    public class EditorCategoryViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must contain between 3 to 50 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Slug is required")]
+        public string Slug { get; set; }
+
+        public static implicit operator Category(EditorCategoryViewModel model)
+        {
+            return new Category
+            {
+                Name = model.Name,
+                Slug = model.Slug.ToLower()
+            };
+        }
+    }
+}
